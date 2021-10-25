5cba50ad1790113c8f65e5932ebb152c8fa3a28a
andrey <andrey@ANDREY-PC>
2021-10-25 11:34:59 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1Grupo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio1Grupo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo p1 = new Retangulo();
+            p1.Area();
+            p1.Perimetro();
+            p1.Diagonal();
+
+            
+        }
+
+       
+    }
+}
---
+++ Exercicio1Grupo/Retangulo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio1Grupo
+{
+	public class Retangulo
+	{
+		private double Largura = 3;
+
+		private double Altura = 4;
+
+		public void Area()
+		{
+			double area = Largura * Altura;
+			Console.WriteLine("Area = {0}", area);
+
+		}
+
+        public void Perimetro()
+		{
+			double perimetro = Largura+Largura+Altura+Altura;
+			Console.WriteLine("Perimetro = {0}", perimetro);
+
+		}
+
+        public void Diagonal()
+		{
+			double diagonal = Math.Pow(Largura,2) + Math.Pow(Altura,2);
+			diagonal = Math.Sqrt(diagonal);
+			Console.WriteLine("Diagonal = {0}.N2", diagonal);
+
+		}
+	}
+}
+
